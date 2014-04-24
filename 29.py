3806d4a6ab3685da22adc6244d597b6ad6cc22ad
Dawei Xu <dawei.xu@gmail.com>
2014-04-24 21:24:03 +0800

    29

--- 29.py
+++ 29.py
@@ -1,20 +1,14 @@
 import math
-high = 100
-low = 2
-total = int(math.pow((high-low+1),2))
+_high = 100
+_low = 2
 
-for i in range(low,int(math.sqrt(high))+1):
-    for j in range(low,high+1):
-        if math.pow(i,j) <= high:
-            total -= high//j - 1
-        else:
-           break
 
-print(total)
 
-result = []
-for i in range(low, high+1):
-    for j in range(low, high+1):
-        result.append(int(math.pow(i,j)))
+def getCountByCompuation(low, high):
+    result = set([])
+    for i in range(low, high+1):
+        for j in range(low, high+1):
+            result.add(int(math.pow(i,j)))
+    return len(result)
 
-print(len(set(result)))
+print(getCountByCompuation(_low, _high))
