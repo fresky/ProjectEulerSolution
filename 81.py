be7e493e1d95a2648fe6ff3852238f0fdcf5aebd
fresky <dawei.xu@gmail.com>
2014-04-26 09:02:02 +0800

    81

---
+++ 81.py
@@ -0,0 +1,29 @@
+file_object = open('81.txt', 'r')
+triangle = file_object.readlines()
+
+row = len(triangle)
+
+array = []
+for line in triangle:
+    array += [int(s) for s in line.split(',')]
+
+import math
+import sys
+row = int(math.sqrt(len(array)))
+
+
+for i in range(row):
+    for j in range(row):
+        if i == 0 and j == 0:
+            continue
+        if j > 0:
+            left = array[i * row + j] + array[i * row + j - 1]
+        else:
+            left = sys.maxsize
+        if i > 0:
+            above = array[i * row + j] + array[(i - 1) * row + j]
+        else:
+            above = sys.maxsize
+        array[i*row+j]=min(left, above)
+
+print(array[-1])
