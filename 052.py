a2b4b14d23f8116835d99851142341cf744ce819
Dawei Xu <dawei.xu@gmail.com>
2014-05-09 17:55:30 +0800

    52

---
+++ 052.py
@@ -0,0 +1,40 @@
+import math
+import time
+
+start = time.time()
+
+def is_valid(num, ori_set):
+    cur_set = set()
+    while num > 0:
+        if num%10 not in ori_set:
+            return False
+        cur_set.add(num%10)
+        num = num//10
+    return cur_set == ori_set
+
+def get_permuted():
+    digit = 1
+    while True:
+        begin = 10**(digit-1)
+        limit = 10**digit//6 + 1
+        for i in range(begin, limit):
+            if i == 0:
+                continue
+            set1 = set()
+            num = i
+            while num > 0:
+                set1.add(num%10)
+                num = num//10
+            found = True
+            for j in range(2,7):
+                if not is_valid(j*i, set1):
+                    found = False
+                    break
+            if found:
+                return i
+        digit +=1
+
+print(get_permuted())
+
+
+
